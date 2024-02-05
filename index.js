2c42dc50bfdd3b5171f09154fe4c47a31b1cc4dd
rishabh3072 <143130428+rishabh3072@users.noreply.github.com>
2024-02-05 17:24:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var rishabh=Math.floor(Math.random()*6)+1;
 
 var randomImage = "dice" + rishabh + ".png";
-var randImgSrc = "images/" + randomImage;
-document.querySelectorAll("img")[0].setAttribute("src", randImgSrc);
+document.querySelectorAll("img")[0].setAttribute("src", randomImage);
 
 var bhavishya=Math.floor(Math.random()*6)+1;
 
 var randomImage2 = "dice" + bhavishya + ".png";
-var randImgSrc2 = "images/" + randomImage2;
-document.querySelectorAll("img")[1].setAttribute("src", randImgSrc2);
+document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
 
 if(rishabh>bhavishya)
 {
